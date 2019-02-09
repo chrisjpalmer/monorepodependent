5eb064ff868c75c90d24821c12f0823c816cd9ff
Christopher Palmer <chrisjpalmer@optusnet.com.au>
2019-02-09 20:22:59 +1100

    added the pkg1 as a dependency

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 import "github.com/chrisjpalmer/monorepotest/pkg2"
+import "github.com/chrisjpalmer/monorepotest/pkg1"
 
 func main() {
 	fmt.Println("This is app3")
 	pkg2.Pkg2Function()
+	pkg1.Pkg1Function()
 }
